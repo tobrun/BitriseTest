0fd3fbe629e3ed91b622d4bd4f45206ba379988d
Tobrun <tobrun.van.nuland@gmail.com>
2016-06-17 13:23:55 +0200

    add test for running instrumentation tests on Bitrise

---
+++ BitriseTest/app/src/androidTest/java/com/tobrun/bitrisetest/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.tobrun.bitrisetest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.util.Log;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Nurbot on 03/12/15.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+    @Before
+    public void setActivity() {
+        mActivity = mActivityRule.getActivity();
+    }
+
+    @Test
+    public void testSanity() {
+        Log.v("TAG", "Test sanity");
+        onView(withId(android.R.id.content)).check(matches(isDisplayed()));
+    }
+}
